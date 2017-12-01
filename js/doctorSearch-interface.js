937b4e8431a430845c7b5c36e06df13964c4a1ff
Susannah Lowe <lowesusannah@gmail.com>
2017-12-01 13:15:07 -0800

    add API call to doctorSearch-interface.js. Add variables which correctly for the user's search input and for the API key to the url which is being called. Tests show correct results for search input of john in portland area (the query value of the returned JSON object

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -6,13 +6,38 @@ $(document).ready(function(){
   $("#output").hide();
   $('#submit').click(function(){
     event.preventDefault();
+    let inputString = $('#search-string-input').val();
+    // let results = new DoctorSearch(inputString);
+    // function -- if this.responseText - parsed into words (split(' ')).contains `${inputString}` then return/append item to list. Do a for loop through items and append each result name, address and website if responseText .contains the input string word;
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&sort=rating-desc&skip=0&limit=25&user_key=${apiKey}`;
+
+    alert(url);
+
+
     $("#input").hide();
     $("#output").show();
-    let inputString = $('#search-string-input').val();
-    $("#outcome").text(inputString);
-    // let results = new DoctorSearch(inputString);
-    // results.responses();
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+
+    request.open("GET", url, true);
+    request.send();
+
+    getElements = function(response) {
+      alert("got to get Elements");
+      alert("this.responseText");
+      // $('#outcome').text(`${response.data.practices.name}`);
+    }
+
     // $('#outcome').text(results.responses());
+    // $("#outcome").text(inputString);
+    // results.responses();
   });
 });
 
