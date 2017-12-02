880f71458d25941bd528a09219bd32d0e2135d92
Susannah Lowe <lowesusannah@gmail.com>
2017-12-01 17:01:00 -0800

    set up new instance of Promise object, and returned Promise to handle asynchronous code

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -1,52 +1,42 @@
 var apiKey = require('./../.env').apiKey;
 
-
 import { DoctorSearch } from './../js/doctorSearch.js';
 $(document).ready(function(){
   $("#output").hide();
   $('#submit').click(function(){
     event.preventDefault();
-    let inputString = $('#search-string-input').val();
-    // let results = new DoctorSearch(inputString);
-    // function -- if this.responseText - parsed into words (split(' ')).contains `${inputString}` then return/append item to list. Do a for loop through items and append each result name, address and website if responseText .contains the input string word;
-    let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
-
     $("#input").hide();
-    $("#output").show();
-
-    const getElements = function(response) {
-      $('#outcome').text(`${response.data.practices.name}`);
-      // $('#outcome').text(`${response.data.practices.name}`);
-    };
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        getElements(response);
+    let inputString = $('#search-string-input').val("");
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
       }
     };
-
-
-    request.open("GET", url, true);
-    request.send();
-
-
-    // $('#outcome').text(results.responses());
-    // $("#outcome").text(inputString);
-    // results.responses();
-  });
+  request.open ("GET", url, true);
+  request.send();
 });
 
-
-  // $('#droppable').droppable({
-  //   drop: function() {
-  //    if $('draggable').hasClass('small') {
-  //      whatever.eatSmall();
-  //    } else {
-  //      if $('draggable').hasClass('medium') {
-  //        whatever.eatMedium();
-  //      }
-  //    }
-  //
-  //     $('#outcome').text(whatever.foodLevel);
-  //   }
+  promise.then(function(response) {
+    $("#output").show();
+    let body = JSON.parse(response);
+    alert(JSON.parse(response));
+      if (`${body.response.returnResults}` === true) {
+        alert("true");
+        $('#show-name').text(`${body.main.data.practices.name}`);
+        $('#show-website').text(`${body.main.data.practices.website}`);
+      } else {
+        $('#show-name').hide();
+        $('#show-website').hide();
+        alert("no responses for search");
+        $('#output').append("Sorry, your search returned 0 results. Please check your spelling, or try making your search criteria more general, and try again");
+      }
+  }, function(error) {
+    $("#show-error").text(`There was an error processing your request`);
+    });
+  });
+});
--- js/doctorSearch.js
+++ js/doctorSearch.js
@@ -1,77 +1,14 @@
 export class doctorSearch {
 
-  constructor(name) {
-    this.name = name;
-    this.foodLevel = 10;
+  constructor(inputString) {
+    this.inputString = inputString;
+  }
+
+  returnResults() {
+    if (this.body.main.meta.total === 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
-  // setHunger() {
-  //   setInterval(() => {
-  //     this.foodLevel--;
-  //   }, 1000);
-  //   alert("Working");
-  //   return this.foodLevel;
-  // }
-
-  // setHunger() {
-  //   setInterval(() => {
-  //     this.foodLevel--;
-  //   }, 1000);
-  //   setInterval(() => {
-  //     return this.foodLevel;
-  //   }, 1000);
-  //
-  //
-  // }
-
-
-
-  // didYouGetEaten() {
-  //   if (this.foodLevel > 0) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
-
-  // youNotEaten() {
-  //   if (this.foodLevel > 0){
-  //     return true;
-  //   }
-  // }
-  //
-  // youGotEaten() {
-  //   this.foodLevel = 0;
-  //   if (this.foodLevel <= 0){
-  //     return true;
-  //   }
-  // }
-
-//   feed() {
-//     this.foodLevel = 10;
-//   }
-//
-//   eatSmall() {
-//     this.foodLevel +=5;
-//     return this.foodLevel;
-//   }
-//
-//   eatMedium() {
-//     this.foodLevel +=7;
-//     return this.foodLevel;
-//   }
-//
-//   eatBig() {
-//     this.foodLevel +=10;
-//     return this.foodLevel;
-//   }
-//
-//   eatHuman() {
-//     this.foodLevel +=20;
-//     return this.foodLevel;
-//   }
-//
-//   eatChocolateDoughnut() {
-//     this.foodLevel +=30;
-//     return this.foodLevel;
-//   }
