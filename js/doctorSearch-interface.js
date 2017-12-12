30b8a492554c8f17e5d4949dd9e15e405d50639e
Susannah Lowe <lowesusannah@gmail.com>
2017-12-11 21:28:09 -0800

    refactor to separate user interface from business logic javascript.

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -1,37 +1,27 @@
-var apiKey = require('./../.env').apiKey;
-
 import { DoctorSearch } from './../js/doctorSearch.js';
 
+// let displayData = function(results) {
+//   results.forEach(function(result) {
+//     $('#output').append('<li>' + result.name + '</li>');
+//   });
+// };
 
+$("#output").hide();
 $(document).ready(function(){
-  $("#output").hide();
   $('#submit').click(function(){
     event.preventDefault();
-    $("#input").hide();
-    let inputString = $('#search-string-input').val("");
-    // let search = new DoctorSearch(inputString);
-    let promise = new Promise(function(resolve, reject) {
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
-      const request = new XMLHttpRequest();
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-      }
-    };
-    request.open ("GET", url, true);
-    request.send();
-});
 
-  promise.then(function(response) {
-    $("#output").show();
-      let body = JSON.parse(response);
-      alert(`${body.data.practices[0].name}`);
-      alert(`${body.data.practices[0].website}`);
-    // $('#show-name').text(`${body}`);
-  }, function(error) {
-    $("#show-error").text(`There was an error processing your request`);
+    $("#input").hide();
+    $('#output').show();
+    let inputString = $('#search-string-input').val("");
+    applicationModule.getData(userInput, displayData);
+
+      promise.then(function(response) {
+        let body = JSON.parse(response);
+        alert(`${body.data.practices[0].name}`);
+        alert(`${body.data.practices[0].website}`);
+        }, function(error) {
+        alert(`There was an error processing your request`);
+      });
     });
   });
-});
--- js/doctorSearch.js
+++ js/doctorSearch.js
@@ -1,14 +1,22 @@
-export class doctorSearch {
+var apiKey = require('./../.env').apiKey;
 
+
+export class doctorSearch {
   constructor(inputString) {
     this.inputString = inputString;
   }
+  let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
 
-  // returnResults(inputString) {
-  //   if (this.body.main.meta.total === 0) {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open ("GET", url, true);
+    request.send();
+  });
 }
