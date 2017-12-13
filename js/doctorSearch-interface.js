7121d977541fe9e8947d5c7b79509966b2e7bde9
Susannah Lowe <lowesusannah@gmail.com>
2017-12-12 19:43:44 -0800

    refactored user interface logic after chatting with Lina

--- js/doctorSearch-interface.js
+++ js/doctorSearch-interface.js
@@ -1,27 +1,18 @@
 import { DoctorSearch } from './../js/doctorSearch.js';
 
-// let displayData = function(results) {
-//   results.forEach(function(result) {
-//     $('#output').append('<li>' + result.name + '</li>');
-//   });
-// };
+let displayData = function(results) {
+    $('#output').append('<p>' + results + '</p>');
+};
 
-$("#output").hide();
+
 $(document).ready(function(){
+  $("#output").hide();
   $('#submit').click(function(){
     event.preventDefault();
-
     $("#input").hide();
     $('#output').show();
-    let inputString = $('#search-string-input').val("");
-    applicationModule.getData(userInput, displayData);
-
-      promise.then(function(response) {
-        let body = JSON.parse(response);
-        alert(`${body.data.practices[0].name}`);
-        alert(`${body.data.practices[0].website}`);
-        }, function(error) {
-        alert(`There was an error processing your request`);
-      });
-    });
+    let inputString = $('#search-string-input').val();
+    let doctorSearch = new DoctorSearch(inputString);
+    doctorSearch.getData(displayData);
   });
+});
--- js/doctorSearch.js
+++ js/doctorSearch.js
@@ -1,22 +1,33 @@
 var apiKey = require('./../.env').apiKey;
 
 
-export class doctorSearch {
+export class DoctorSearch {
   constructor(inputString) {
     this.inputString = inputString;
   }
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
 
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open ("GET", url, true);
-    request.send();
-  });
+   getData(displayData) {
+    let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.uiString}&location=or-portland&user_location=45.523%2C122.676&skip=0&limit=10&user_key=${apiKey}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+
+      request.open ("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+    body = JSON.parse(response);
+      results(body);
+    }, function(error) {
+    return (`There was an error processing your request`);
+    });
+  }
 }
